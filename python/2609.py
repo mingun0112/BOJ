c635291a80b432fe09fc912f8a47e51fa5c9a016
정민균 <mingyun@jeongmingyun-ui-MacBookPro.local>
2025-01-18 19:10:27 +0900

    2609

--- python/2609.py
+++ python/2609.py
@@ -1,3 +1,11 @@
-a, b = input("").split()
-
-print(a,b)
+n = input().split()
+n = sorted(list(map(int, n)))
+a, b = n[0], n[1]
+gcd = 0
+# print(a, b)
+for i in range(a, 0, -1):
+    if a % i == 0 and b % i == 0:
+        gcd = i
+        break
+print(gcd)
+print(a*b//gcd)
