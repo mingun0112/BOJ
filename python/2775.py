4fc7dd127ed6cd6a8cf6486e6be6ce9015782c8c
정민균 <mingyun@jeongmingyun-ui-MacBookPro.local>
2025-01-18 19:59:22 +0900

    2775

---
+++ python/2775.py
@@ -0,0 +1,13 @@
+t = int(input(""))
+
+
+
+for i in range(t):
+    k = int(input())
+    n = int(input())
+    f0 = [x for x in range(1,n+1)]
+    for k_1 in range(k):
+        for n_1 in range(1, n):
+            f0[n_1] += f0[n_1-1]
+print(f0[-1])
+    
