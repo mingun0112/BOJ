989925a2e41b598a75a7dc831e8e97a8c920d698
정민균 <mingyun@jeongmingyun-ui-MacBookPro.local>
2025-01-20 18:50:49 +0900

    1920

---
+++ python/1920.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+a = list(map(int, input().split()))
+a= set(a)
+print(a)
+m = int(input())
+
+b = list(map(int, input().split()))
+
+for i in b:
+    if i in a:
+        print(1)
+    else:
+        print(0)
